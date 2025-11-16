031c344cf0323953a8c746dada997b68470ce456
jacksonspenc <jacksonspencer327@gmail.com>
2025-11-16 11:25:20 -0700

    commit 1

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        int userNumber = -1;
+
+        //keep asking for numbers if they dont press 0
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            userNumber = int.Parse(input);
+
+            //add them if its not zero
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        //Compute the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        //Compute the average
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //Find the max
+         int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
