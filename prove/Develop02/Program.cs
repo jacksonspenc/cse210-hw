86f026d911d9f5f7eac965eae25875afd8f4f638
jacksonspenc <jacksonspencer327@gmail.com>
2025-11-16 12:08:13 -0700

    commit 1

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,152 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Entry
+{
+    public string _date;
+    public string _promptText;
+    public string _entryText;
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {_date} - Prompt: {_promptText}");
+        Console.WriteLine($"{_entryText}");
+        Console.WriteLine();
+    }
+}
+
+class PromptGenerator
+{
+    public List<string> _prompts;
+
+    public PromptGenerator()
+    {
+        _prompts = new List<string>
+        {
+            "What was the highlight of the day?",
+            "How can I make tomorrow even better?",
+            "What makes me feel excited to get up in the morning?",
+            "What is something I need to stop doing and start doing?",
+            "What is a habit I would like to stop doing?",
+            "What is something I could do to make today amazing?",
+            "Tell me about a time you were embarrassed."
+        };
+    }
+
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+}
+
+class Journal
+{
+    public List<Entry> _entries;
+
+    public Journal()
+    {
+        _entries = new List<Entry>();
+    }
+
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string file)
+    {
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}~|~{entry._promptText}~|~{entry._entryText}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string file)
+    {
+        _entries.Clear();
+        string[] lines = File.ReadAllLines(file);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("~|~");
+
+            Entry entry = new Entry();
+            entry._date = parts[0];
+            entry._promptText = parts[1];
+            entry._entryText = parts[2];
+
+            _entries.Add(entry);
+        }
+    }
+}
+
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal theJournal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        string choice = "";
+
+        while (choice != "5")
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Entry entry = new Entry();
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._promptText = promptGenerator.GetRandomPrompt();
+
+                Console.WriteLine(entry._promptText);
+                Console.Write("> ");
+                entry._entryText = Console.ReadLine();
+
+                theJournal.AddEntry(entry);
+            }
+            else if (choice == "2")
+            {
+                theJournal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                theJournal.LoadFromFile(filename);
+            }
+            else if (choice == "4")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                theJournal.SaveToFile(filename);
+            }
+            else if (choice == "5")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+        }
     }
 }
